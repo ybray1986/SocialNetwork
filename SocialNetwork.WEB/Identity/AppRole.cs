015d084e237763e668d0a89bd72f33bcb58b3a34
User <User@GL-424-01>
2019-09-30 18:20:01 +0600

    identity types added

---
+++ SocialNetwork.WEB/Identity/AppRole.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialNetwork.WEB.Identity
+{
+    public class AppRole: IdentityRole
+    {
+        //https://stackoverflow.com/questions/31960433/adding-asp-net-mvc5-identity-authentication-to-an-existing-project
+
+    }
+}
