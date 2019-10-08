1b5623ede2f88ad6c9d48f718de4f3d938e6074c
Аман Ыбрай <ybray1986@gmial.com>
2019-10-08 20:01:25 +0600

    conf web.conf

--- SocialNetwork.AUTH/Infrastucture/AuthProvider.cs
+++ SocialNetwork.AUTH/Infrastucture/AuthProvider.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SocialNetwork.AUTH.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace SocialNetwork.AUTH.Infrastucture
 {
-    class AuthProvider<T>:IAuthProvider<T> where T: class
+    class AuthProvider:IAuthProvider<AppUser>
     {
     }
 }
