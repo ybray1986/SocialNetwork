2a92c7a0fc5f1b1d547a53e83e5f3e810c5d1b0e
Аман Ыбрай <ybray1986@gmial.com>
2019-10-09 20:20:30 +0600

    register, login added

---
+++ SocialNetwork.WEB/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialNetwork.WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                ViewBag.Result = User.Identity.Name;
+            }
+            else
+            {
+                ViewBag.Result = "You are not logged in!";
+            }
+            return View();
+        }
+    }
+}
