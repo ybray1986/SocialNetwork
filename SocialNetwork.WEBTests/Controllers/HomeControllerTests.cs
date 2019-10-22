2e4d48535b1a317bcb44df97235249a5f646b398
Аман Ыбрай <ybray1986@gmail.com>
2019-10-22 14:23:08 +0600

    test

---
+++ SocialNetwork.WEBTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SocialNetwork.WEB.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using AutoMapper;
+
+namespace SocialNetwork.WEB.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+
+        [TestMethod()]
+        public void AddContentTest()
+        {
+            var repo = new Mock<IMapper>();
+            repo.Setup(d => d.ServiceCtor.Invoke(typeof(HomeControllerTests)));
+            var controller = new HomeController(repo);
+            var result = controller.Index() as ViewResult;
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
---
+++ SocialNetwork.WEBTests1/Controllers/TestControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SocialNetwork.WEB.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SocialNetwork.WEB.Controllers.Tests
+{
+    [TestClass()]
+    public class TestControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            var controller = new TestController();
+            var result = controller.Details(2) as ViewResult;
+            Assert.AreEqual("Details", result.ViewName);
+        }
+    }
+}
